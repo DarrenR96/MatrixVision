31dc5d736687905e6c879b138fc19795cbe641a9
Darren Ramsook <43309308+DarrenR96@users.noreply.github.com>
2022-10-25 11:47:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ cv2.namedWindow('Matrix Vision')
 while(True):
     ret, frame = vid.read()
     matrixFrame = YtoASCII(frame,t=85)
+    height, width = frame.shape[0], frame.shape[1]
     frame = cv2.rectangle(frame,(0,600),(1280,720),(0,0,0),-1)
     cv2.putText(frame,'SigMedia Team, Electronic & Electrical Engineering', (60,675), cv2.FONT_HERSHEY_SIMPLEX, 1.4, (255,255,255), 1, 2)
     cv2.imshow('Camera Input', frame)
